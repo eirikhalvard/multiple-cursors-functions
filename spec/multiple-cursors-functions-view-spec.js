3f079474decc9ff79f8fdf78903cf8140bb4ef3e
Eirik Halvard Sæther <eirik.halvard.95@gmail.com>
2018-07-31 01:12:47 +0200

    [REMOVED] specs

--- spec/multiple-cursors-functions-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import MultipleCursorsFunctionsView from '../lib/multiple-cursors-functions-view';
-
-describe('MultipleCursorsFunctionsView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
